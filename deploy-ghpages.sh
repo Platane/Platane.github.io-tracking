9328a35c5b2d4bf4c46fce69cc674097eec6ed22
Platane <brongniart.arthur@gmail.com>
2015-07-30 21:54:30 +0200

    deploy script

---
+++ deploy-ghpages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git stash
+git checkout gh-pages
+git pull
+git checkout master -- .
+npm install
+cp ./front-dist/* ./
+cp ./front-src/index.html ./
+cp --force ./.gitignore-ghpages ./.gitignore
+git add .
+git comit -m "deploy"
+git push
+git checkout master
