11fd934b1ce9425ef742c48fa28cf76854dd54e4
Platane <brongniart.arthur@gmail.com>
2015-07-30 22:43:42 +0200

    deploy build

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+
+# stash and change branch to gh-pages
 git stash
 git checkout gh-pages
 git pull
+
+# grab the lastest files from master
 git checkout master -- .
+
+# build
+rm -rf ./front-dist/*
 npm install
+
+# copy dist files to root dir
 cp ./front-dist/* ./
 cp ./front-src/index.html ./
+
+# keep ignoring files
 git checkout -- ./gitignore
 git rm -r --cached .
 git add .
+
+# commit and push
 git commit -m "deploy"
 git push
+
+# back to master
 git checkout master
