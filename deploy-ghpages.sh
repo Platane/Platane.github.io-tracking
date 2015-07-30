5ae1db42338033ae661dfc631e798ce334280928
Platane <brongniart.arthur@gmail.com>
2015-07-30 22:38:44 +0200

    fix build

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -6,8 +6,8 @@ git checkout master -- .
 npm install
 cp ./front-dist/* ./
 cp ./front-src/index.html ./
-cp --force ./.gitignore-ghpages ./.gitignore
-git rm - --cached .
+git checkout -- ./gitignore
+git rm -r --cached .
 git add .
 git commit -m "deploy"
 git push
