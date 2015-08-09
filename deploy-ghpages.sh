c5e8ddc0d944446fb04ed18722a5b76f22a23d17
Platane <brongniart.arthur@gmail.com>
2015-08-09 16:47:56 +0200

    Update deploy-ghpages.sh

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -21,13 +21,7 @@ cp ./front-src/index.html ./
 # keep ignoring files
 printf "front-dist\nfront-src\nrun.js\nwebpack.config.js\n.gitignore-ghpages\nnode_modules\nnpm-debug.log\nREADME.md" > .gitignore
 git rm -r --cached .
-# /!\ can't do git add . here because the .sh script is running,
-#   it will fail with no permission as the file is locked
-#   let's add everything but the .sh script
-git add ./*.js
-git add ./*.html
-git add ./.gitignore
-git add ./package.json
+git add .
 
 # commit and push
 git commit -m "deploy"
