3098479cf4e30304fce8f5c42113572d21d071b0
Platane <brongniart.arthur@gmail.com>
2015-08-09 16:38:43 +0200

    change deploy script

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -6,7 +6,9 @@ git checkout gh-pages
 git pull
 
 # grab the lastest files from master
-git checkout master -- .
+git checkout master -- ./front-src
+git checkout master -- ./package.json
+git checkout master -- ./webpack.config.js
 
 # build
 rm -rf ./front-dist/*
@@ -17,7 +19,7 @@ cp ./front-dist/* ./
 cp ./front-src/index.html ./
 
 # keep ignoring files
-printf "\nfront-dist\nfront-src\nrun.js\nwebpack.config.js\n.gitignore-ghpages\nnode_modules\nnpm-debug.log" > .gitignore
+printf "\nfront-dist\nfront-src\nrun.js\nwebpack.config.js\n.gitignore-ghpages\nnode_modules\nnpm-debug.log\nREADME.md" > .gitignore
 git rm -r --cached .
 # /!\ can't do git add . here because the .sh script is running,
 #   it will fail with no permission as the file is locked
@@ -25,7 +27,6 @@ git rm -r --cached .
 git add ./*.js
 git add ./*.html
 git add ./.gitignore
-git add ./README.md
 git add ./package.json
 
 # commit and push
